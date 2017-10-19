d736680fc6962d0b0a4aa06ca544d781190d5e51
kkycj <kkycj@vip.qq.com>
2017-10-19 19:23:30 +0800

    ver 2.0
    删除早期草稿
    增加4种语言
    增加帮助界面

--- translator.py
+++ translator.py
@@ -9,89 +9,108 @@ def check_contain_chinese(check_str):
 	if check_str>= u"\u4e00" and check_str<= u"\u9fa6":
 		rt = True
 	return rt
-#日文检测
-def check_contain_japanese(check_str):
-	rt = False
-	if check_str>= u"\u3040" and check_str<= u"\u30ff":
-		rt = True
-	return rt
 
-print("""----------肉丸翻译机----------
-	支持中译英和英译中""")
+print("""
+******************************
+ ------欢迎使用肉丸翻译机------
 
+ 使用帮助：
+	 支持中文⇦⇨外文翻译转换
+	 
+	 支持长文翻译
+
+	 支持英、日、德、法、西、俄、韩六国语言
+
+	 输入“!back”返回模式菜单
+ 	 *注意*“!”为英文字符
+
+******************************
+	""")
 while True:
-	mode = input("""请选择模式：
-		1.中英
-		2.中日
-		:""")
+	try:
+		mode = input("""请输入数字选择模式：
 
-	if mode  == '1':	
-		while True:
+1.中英
+2.中日
+3.中德
+4.中法
+5.中西
+6.中俄
+7.中韩
+
+模式:""")
+		mode = int(mode)
+		while True:	
 			txt = input("请输入文本：")
+			url = 'http://fanyi.baidu.com/v2transapi'
+			data = {
+				'from':'',
+				'to':'',
+				'query':txt,
+				'transtype':'translang',
+				'simple_means_flag':'3'
+			}
 			if txt == '!back':
 				break
-			if check_contain_chinese(txt):
-				url = 'http://fanyi.baidu.com/v2transapi'       
-				data = {
-				'from':'zh',
-				'to':'en',
-				'query':txt,
-				'transtype':'translang',
-				'simple_means_flag':'3'
-				}
-			else:
-				url = 'http://fanyi.baidu.com/v2transapi'       
-				data = {
-				'from':'en',
-				'to':'zh',
-				'query':txt,
-				'transtype':'translang',
-				'simple_means_flag':'3'
-				}
-			data = urllib.parse.urlencode(data).encode('utf-8')
-			response = urllib.request.urlopen(url,data)
-			html = response.read().decode('utf-8')
-			html = json.loads(html)
-			trans = html['trans_result']['data'][0]['dst']
-			print("翻译结果：",trans)
-	if mode == '2':
-		while True:
-			txt = input("请输入文本：")
-			if txt == '!back':
-				break
-			if check_contain_japanese(txt):
-				#url = 'https://dict.hjenglish.com/services/Translate.ashx'       
-				#txt = urllib.parse.quote(txt)
-				#data = {'from':'ja','to':'zh-CN','txt':txt,}
-				url = 'http://fanyi.baidu.com/v2transapi'       
-				data = {
-				'from':'jp',
-				'to':'zh',
-				'query':txt,
-				'transtype':'translang',
-				'simple_means_flag':'3'
-				}
-			else:
-				#url = 'https://dict.hjenglish.com/services/Translate.ashx'       
-				#txt = urllib.parse.quote(txt)
-				#data = {'from':'zh-CN','to':'ja','txt':txt,}
-				url = 'http://fanyi.baidu.com/v2transapi'       
-				data = {
-				'from':'zh',
-				'to':'jp',
-				'query':txt,
-				'transtype':'translang',
-				'simple_means_flag':'3'
-				}
+			elif mode == 1:
+				if check_contain_chinese(txt):     
+					data['from'] = 'zh'
+					data['to'] = 'en'
+				else:
+					data['from'] = 'en'
+					data['to'] = 'zh'
+			elif mode == 2:
+				if check_contain_chinese(txt):
+					data['from'] = 'zh'
+					data['to'] = 'jp'
+				else:
+					data['from'] = 'jp'
+					data['to'] = 'zh'
+			elif mode == 3:
+				if check_contain_chinese(txt):
+					data['from'] = 'zh'
+					data['to'] = 'de'
+				else:
+					data['from'] = 'de'
+					data['to'] = 'zh'
+			elif mode == 4:
+				if check_contain_chinese(txt):
+					data['from'] = 'zh'
+					data['to'] = 'fra'
+				else:
+					data['from'] = 'fra'
+					data['to'] = 'zh'
+			elif mode == 5:
+				if check_contain_chinese(txt):
+					data['from'] = 'zh'
+					data['to'] = 'spa'
+				else:
+					data['from'] = 'spa'
+					data['to'] = 'zh'
+			elif mode == 6:
+				if check_contain_chinese(txt):
+					data['from'] = 'zh'
+					data['to'] = 'ru'
+				else:
+					data['from'] = 'ru'
+					data['to'] = 'zh'
+			elif mode == 7:
+				if check_contain_chinese(txt):
+					data['from'] = 'zh'
+					data['to'] = 'kor'
+				else:
+					data['from'] = 'jp'
+					data['to'] = 'kor'
 			data = urllib.parse.urlencode(data).encode('utf-8')
 			#主要是由于该网站禁止爬虫导致的，可以在请求加上头信息，伪装成浏览器访问User-Agent
-			#headers = {'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0'}
 			#headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
-			#req = urllib.request.Request(url, headers=headers)  
+			#req = urllib.request.Request(url, headers=headers)
+			#response = urllib.request.urlopen(req) 
 			response = urllib.request.urlopen(url,data)
-			#response = urllib.request.urlopen(req)
 			html = response.read().decode('utf-8')
 			html = json.loads(html)
-			#print(html)
-			trans = html['trans_result']['data'][0]['dst']
-			print("翻译结果：",trans)
+			result = html['trans_result']['data'][0]['dst']
+			print("翻译结果：",result)
+	except:
+		print("*输入有误，请重新输入！*")
+		continue
