08eb8fae9d7f8075ec116f30b05db68dd46e0b2d
kkycj <kkycj@vip.qq.com>
2017-10-19 18:31:55 +0800

    精简代码
    2精简了重复代码

--- translator2.py
+++ translator2.py
@@ -17,63 +17,46 @@ def check_contain_japanese(check_str):
 	return rt
 
 print("""----------肉丸翻译机----------
-	支持中译英和英译中""")
+
+	""")
 while True:
 	mode = input("""请选择模式：
 		1.中英
 		2.中日
 	模式:""")
-	if mode  == '1':	
-		while True:
-			txt = input("请输入文本：")
-			url = 'http://fanyi.baidu.com/v2transapi'
-			data = {
-				'from':'',
-				'to':'',
-				'query':txt,
-				'transtype':'translang',
-				'simple_means_flag':'3'
-			}
-			if txt == '!back':
-				break
+	while True:	
+		txt = input("请输入文本：")
+		url = 'http://fanyi.baidu.com/v2transapi'
+		data = {
+			'from':'',
+			'to':'',
+			'query':txt,
+			'transtype':'translang',
+			'simple_means_flag':'3'
+		}
+		if txt == '!back':
+			break
+		if mode == '1':
 			if check_contain_chinese(txt):     
 				data['from'] = 'zh'
 				data['to'] = 'en'
 			else:
 				data['from'] = 'en'
 				data['to'] = 'zh'
-			data = urllib.parse.urlencode(data).encode('utf-8')
-			#主要是由于该网站禁止爬虫导致的，可以在请求加上头信息，伪装成浏览器访问User-Agent
-			#headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
-			#req = urllib.request.Request(url, headers=headers)
-			#response = urllib.request.urlopen(req) 
-			response = urllib.request.urlopen(url,data)
-			html = response.read().decode('utf-8')
-			html = json.loads(html)
-			result = html['trans_result']['data'][0]['dst']
-			print("翻译结果：",result)
-	if mode == '2':
-		while True:
-			txt = input("请输入文本：")
-			url = 'http://fanyi.baidu.com/v2transapi'
-			data = {
-				'from':'',
-				'to':'',
-				'query':txt,
-				'transtype':'translang',
-				'simple_means_flag':'3'
-			}
-			if txt == '!back':
-				break
+		if mode == '2':
 			if check_contain_japanese(txt):
 				data['from'] = 'jp'
 				data['to'] = 'zh'
 			else:
 				data['from'] = 'zh'
 				data['to'] = 'jp'
-			data = urllib.parse.urlencode(data).encode('utf-8')
-			response = urllib.request.urlopen(url,data)
-			html = response.read().decode('utf-8')
-			html = json.loads(html)
-			result = html['trans_result']['data'][0]['dst']
-			print("翻译结果：",result)
+		data = urllib.parse.urlencode(data).encode('utf-8')
+		#主要是由于该网站禁止爬虫导致的，可以在请求加上头信息，伪装成浏览器访问User-Agent
+		#headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'}
+		#req = urllib.request.Request(url, headers=headers)
+		#response = urllib.request.urlopen(req) 
+		response = urllib.request.urlopen(url,data)
+		html = response.read().decode('utf-8')
+		html = json.loads(html)
+		result = html['trans_result']['data'][0]['dst']
+		print("翻译结果：",result)
